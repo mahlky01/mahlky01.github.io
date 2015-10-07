4aa52e65061a46ab1f3da8c49a003e9a62f5faf8
mahlky01 <mahlky01@luther.edu>
2015-10-07 12:56:17 -0500

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,38 @@
+add = function() {
+        var Num1;
+        var Num2;
+        var Result;
+        Num1 = document.querySelector("#num1").value;
+        Num2 = document.querySelector("#num2").value;
+        Result = parseFloat(Num1) + parseFloat(Num2);
+        document.querySelector("#result").value = Result;
+      }
+     subtract = function() {
+        var Num1;
+        var Num2;
+        var Result;
+        Num1 = document.querySelector("#num1").value;
+        Num2 = document.querySelector("#num2").value;
+        Result = parseFloat(Num1) - parseFloat(Num2);
+        document.querySelector("#result").value = Result;
+      }
+     multiply = function() {
+        var Num1;
+        var Num2;
+        var Result;
+        Num1 = document.querySelector("#num1").value;
+        Num2 = document.querySelector("#num2").value;
+        Result = parseFloat(Num1) * parseFloat(Num2);
+        document.querySelector("#result").value = Result;
+      }
+     divide = function() {
+        var Num1;
+        var Num2;
+        var Result;
+        Num1 = document.querySelector("#num1").value;
+        Num2 = document.querySelector("#num2").value;
+        Result = parseFloat(Num1) / parseFloat(Num2);
+        document.querySelector("#result").value = Result;
+      }
+        
+        
