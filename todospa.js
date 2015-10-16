9234047cdbc7ffa2c71e7c1ba34c8cc7382b1300
mahlky01 <mahlky01@luther.edu>
2015-10-16 13:07:40 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -3,11 +3,15 @@ newItem = function() {
   var li;
   var input;
   var ul;
+  var checkbox;
   ul = document.querySelector(#ul);
   input = document.querySelector(#input).value;
   li = document.createElement("li");
   li.appendChild(document.createTextNode(input));
   ul.appendChild(li);
+  checkbox = document.createElement("input");
+  checkbox.type = checkbox;
+  li.appendChild(checkbox);
   var priority;
   priority = document.querySelector(#priority).value;
   li.style.class = "." + priority;
